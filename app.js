58ffc1867ba96e5b8b7a27946b40018b5e0238bc
emeldo39 <obiemeldo@gmail.com>
2026-05-10 21:49:46 -0230

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+
+let c = a + b;
+console.log("Sum is " + c);
+
+console.log(`Backsticks example: Sum of a and b is ${c}, this is a backstick`); //Example of backsticks
+
+let login = true;
+
+if (login) {
+  console.log("login successful");
+} else {
+  console.log(`login failed`);
+}
